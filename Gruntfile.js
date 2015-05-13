25eba490e36d4401a70403fa5fe00d27eb6890ef
Anton Zhusman <zhusman@allyst.com>
2015-05-13 13:43:30 +0300

    initial structure; build scripts and styles system

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint: {
+          files: ['Gruntfile.js', 'src/js/**/*.js'],
+          options: {
+            globals: {
+              jQuery: true
+            }
+          }
+        },
+
+        browserify: {
+            js: {
+                options: {
+                    browserifyOptions: {
+                        debug: false
+                    }
+                },
+                src: 'src/js/main.js',
+                dest: 'build/main.js'
+            }
+        },
+
+        uglify: {
+            allyst: {
+                files: {
+                    'build/main.min.js': 'build/main.js'
+                }
+            }
+        },
+
+        less: {
+            development: {
+                files: {
+                    'build/styles.css': 'src/styles/main.less'
+                }
+            }
+        },
+
+        cssmin: {
+            allyst: {
+                src: 'build/styles.css',
+                dest: 'build/styles.min.css'
+            }
+        },
+
+        watch: {
+            files: ['src/js/**/*.js', 'src/styles/**/*.less'],
+            tasks: ['jshint']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-css');
+
+    grunt.registerTask('default', ['less', 'cssmin', 'browserify', 'uglify', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# allyst.github.io
+# LLSN Documentation
+
+
+### Development:
+1. run `npm install` for installing dev dependencies
+2. run `grunt` for build scripts and styles
