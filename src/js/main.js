395c91613ebaf7941450852e294507883dc7dfcc
Anton Zhusman <zhusman@allyst.com>
2015-05-13 19:31:12 +0300

    add code highlighter

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,24 @@
 var $ = global.jQuery = require("./vendors/jquery");
+var hljs = require('./vendors/highlight.pack.js');
+
+
 
 $(function () {
-
+    // инициализация подсветки синтаксиса
+    hljs.initHighlightingOnLoad();
+    initToggleCode();
 });
+
+
+function initToggleCode () {
+    var btns = $('.js-lang-example');
+    btns.on('click', function () {
+        var pre = $(this).next('pre');
+        pre.slideToggle(150);
+    });
+}
+
+
+
+
+
